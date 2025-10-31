eda8fcebf7bfdd32cc194e12a365406819c31fe7
Siddhartha Sarkar <72632615+Siddharthasid@users.noreply.github.com>
2025-10-31 20:45:23 +0000

    conter store with casic functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CounterComponent } from './components/counter/counter.component';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AppState } from './states/app.state';
+import { selectCount } from './states/counter/counter.selector';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CounterComponent],
+  imports: [RouterOutlet, CounterComponent, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'CodeSandbox';
+  count$: Observable<number>;
+
+  constructor(private store: Store<AppState>) {
+    this.count$ = this.store.select(selectCount);
+  }
 }
--- src/app/components/counter/counter.component.ts
+++ src/app/components/counter/counter.component.ts
@@ -1,12 +1,37 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppState } from 'src/app/states/app.state';
+import {
+  decrement,
+  increment,
+  reset,
+} from 'src/app/states/counter/counter.actions';
+import { selectCount } from 'src/app/states/counter/counter.selector';
 
 @Component({
   selector: 'app-counter',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './counter.component.html',
-  styleUrl: './counter.component.css'
+  styleUrl: './counter.component.css',
 })
 export class CounterComponent {
+  count$: Observable<number>;
 
+  constructor(private store: Store<AppState>) {
+    this.count$ = this.store.select(selectCount);
+  }
+  increment() {
+    this.store.dispatch(increment());
+  }
+
+  decrement() {
+    this.store.dispatch(decrement());
+  }
+
+  reset() {
+    this.store.dispatch(reset());
+  }
 }
